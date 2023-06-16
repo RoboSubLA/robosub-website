befd3b14dfe5c69f4a8c79510acb7f19f97b76e9
Ashley Tran <73571838+Ashley-Tran@users.noreply.github.com>
2023-06-15 23:48:01 -0700

    Team Subpage styling

--- src/pages/resources/teams/controls.js
+++ src/pages/resources/teams/controls.js
@@ -30,54 +30,55 @@ export default function Controls() {
       {/* Side Nav Bar */}
       <div className={styles.pageWrapper}>
         <div className={styles.navBackground}>
-        <nav className={styles.navbar}>
-          <ul className={styles.navbar_elements}>
-            <button onClick={get_started} className={styles.btn}>
-              GET STARTED
-            </button>
+          <nav className={styles.navbar}>
+            <ul className={styles.navbar_elements}>
+              <button onClick={get_started} className={styles.btn}>
+                GET STARTED
+              </button>
 
-            <button onClick={teamsFunc} className={styles.btn}>
-              {" "}
-              TEAMS{" "}
-            </button>
+              <button onClick={teamsFunc} className={styles.btn}>
+                {" "}
+                TEAMS{" "}
+              </button>
 
-            <div id="dropdown" className={styles.DPContent}>
-              <a href="./framehull">Frame and Hull</a>
-              <a href="./electronics">Electronics</a>
-              <a href="./actuatedsys">Actuated Systems</a>
-              <a href="./controls">Controls</a>
-              <a href="./autonomy">Autonomy</a>
-              <a href="./compvis">Computer Vision</a>
-            </div>
-            <button onClick={arduino} className={styles.btn}>
-              ARDUINO
-            </button>
+              <div id="dropdown" className={styles.DPContent}>
+                <a href="./framehull">Frame and Hull</a>
+                <a href="./electronics">Electronics</a>
+                <a href="./actuatedsys">Actuated Systems</a>
+                <a href="./controls">Controls</a>
+                <a href="./autonomy">Autonomy</a>
+                <a href="./compvis">Computer Vision</a>
+              </div>
+              <button onClick={arduino} className={styles.btn}>
+                ARDUINO
+              </button>
 
-            <button onClick={linuxfunc} className={styles.btn}>
-              LINUX
-            </button>
+              <button onClick={linuxfunc} className={styles.btn}>
+                LINUX
+              </button>
 
-            <button onClick={github} className={styles.btn}>
-              GIT & GITHUB
-            </button>
-          </ul>
-        </nav>
+              <button onClick={github} className={styles.btn}>
+                GIT & GITHUB
+              </button>
+            </ul>
+          </nav>
         </div>
-        
-        <h1>CONTROLS</h1>
-        <p>
-          Controls sits between Autonomy and our actual hardware and turns our
-          high-level movement commands into individual thruster and actuator
-          commands to actually move the robot around. We work mostly in C++ on
-          embedded systems such as our custom controller PCBs, and communicate
-          with the rest of our software over ROS. <br></br>
-          <br></br>Most of the year is spent updating our software to be
-          compatible with new versions of Linux and ROS, and adding any features
-          we think could improve our vehicles' performance. When it comes time
-          to test, we spend a lot of time tuning the parameters of our various
-          orientation and translation controllers to best accomplish commanded
-          movements in the shortest time possible.
-        </p>
+        <div className={styles.myDIV}>
+          <h1 style={{ fontSize: "4rem" }}>CONTROLS</h1>
+          <p>
+            Controls sits between Autonomy and our actual hardware and turns our
+            high-level movement commands into individual thruster and actuator
+            commands to actually move the robot around. We work mostly in C++ on
+            embedded systems such as our custom controller PCBs, and communicate
+            with the rest of our software over ROS. <br></br>
+            <br></br>Most of the year is spent updating our software to be
+            compatible with new versions of Linux and ROS, and adding any
+            features we think could improve our vehicles' performance. When it
+            comes time to test, we spend a lot of time tuning the parameters of
+            our various orientation and translation controllers to best
+            accomplish commanded movements in the shortest time possible.
+          </p>
+        </div>
       </div>
     </Layout>
   );
