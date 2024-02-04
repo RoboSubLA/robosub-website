1e7230af73f1158e74bdf39726287af585261708
Silver1063 <brickbrickerson1063@gmail.com>
2024-02-03 17:44:24 -0800

    Fix remove temp files from repo
    For some reason files that shouldn't be here are, lol. They result in kind useless changes in unrelated commits like "path/on/mysystem.js" to "/path/on/yours.js"

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\dylan\\OneDrive\\Desktop\\robosub-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\dylan\\OneDrive\\Desktop\\robosub-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\dylan\\OneDrive\\Desktop\\robosub-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\dylan\\OneDrive\\Desktop\\robosub-website\\src\\css\\custom.css'),
-];
